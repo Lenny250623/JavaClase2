a4ce65b02b262527ea563e0d7465de242d04c0d0
User <User@DESKTOP-0788V4V>
2024-03-31 19:59:38 -0500

    Pregunta 2

--- src/clasejava2/ClaseJava2.java
+++ src/clasejava2/ClaseJava2.java
@@ -15,6 +15,24 @@ public class ClaseJava2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        final float Pi = 3.141592f;
+        // La fórmuloa es "2π (r h + r²)"
+        
+        // Datos del cilindro 1:
+        float radioCilindro1 = 4.0f;
+        float alturaCilindro1 = 2.5f;
+        float areaCilindro1;
+        
+        // Datos del cilndro 2:
+         float radioCilindro2 = 2.0f;
+         float alturaCilindro2 = 5.0f;
+         float areaCilindro2;
+         
+         areaCilindro1 = 2.0f*Pi*(radioCilindro1*alturaCilindro1 + (float)Math.pow((float)radioCilindro1,2.0));
+         areaCilindro2 = 2.0f*Pi*(radioCilindro2*alturaCilindro2 + (float)Math.pow((float)radioCilindro2,2.0));
+         
+         System.out.println("El area del cilindro 1 es:" + areaCilindro1 + "m²");
+         System.out.println("El area del cilindro 2 es:" + areaCilindro2 + "m²");
     }
     
 }
